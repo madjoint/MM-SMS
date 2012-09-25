f4e7b6eee44e39dfee5711b2466c477b6d09c026
M. H Ansari <mhansari@mmatcher.com>
2012-09-25 15:20:48 +0500

    .

--- mmodules/smsc/xmlpolymerase/__init__.py
+++
@@ -1,4 +0,0 @@
-# package xmlpolymerase
-from .serializer import serialize
-from .deserializer import deserialize
-from .exceptions import PolymeraseError
--- settings.py
+++
@@ -1,8 +0,0 @@
-
-API_DOMAIN = 'api2.mmatcher.com'
-
-REDIS_HOST = 'localhost'
-REDIS_PORT = 6379
-
-REDIS_SMS_DB = 0
-REDIS_API_DB = 2
